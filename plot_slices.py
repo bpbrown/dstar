eb5d2e18f5fb717cead540b72077d3e36b8e6816
Benjamin Brown <bpbrown@gmail.com>
2021-03-10 23:38:20 -0800

    Functioning plot_slices.py for equatorial slices extracted from bulk output.

--- plot_slices.py
+++ plot_slices.py
@@ -6,10 +6,9 @@ Usage:
 
 Options:
     --output=<output>    Output directory; if blank a guess based on likely case name will be made
-    --fields=<fields>    Comma separated list of fields to plot [default: s_fluc,ur,enstrophy]
+    --fields=<fields>    Comma separated list of fields to plot [default: s,enstrophy]
     --dpi=<dpi>          dpi for image files (if png) [default: 300]
-    --fps=<fps>          Frames per second for auto-generated mp4 movie [default: 30]
-    --no_movie           Skip movie making stage
+    --remove_m0          remove m=0 component
 """
 
 import numpy as np
@@ -17,25 +16,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import h5py
-
-def cylinder_plot(r, theta, data, pcm=None, cmap=None):
-    #padded_data = np.pad(data, ((0,0),(0,1),(1,0)), mode='edge')
-    padded_data = data
-    if pcm is None:
-        #r_pad = np.pad(r, ((0,0),(1,1)), mode='constant', constant_values=(0,1))
-        #theta_pad = np.pad(theta, ((1,1), (0,0)), mode='constant', constant_values=(np.pi,0))
-        r_pad = r[None,:]
-        theta_pad = theta[:,None]
-        z, R = r_pad*np.cos(theta_pad), r_pad*np.sin(theta_pad)
-
-        fig, ax = plt.subplots()
-        pcm = ax.pcolormesh(R,z,padded_data[:,:], cmap=cmap)
-        ax.set_aspect(1)
-        return fig, pcm
-    else:
-        pcm.set_array(np.ravel(padded_data[:,:-1]))
-        pcm.set_clim([np.min(data),np.max(data)])
-
 
 def equator_plot(r, phi, data, time=None, index=None, pcm=None, cmap=None, title=None, center_zero=False, norm=1):
     padded_data = data*norm #np.pad(data, ((0,0),(1,0)), mode='edge')
@@ -48,7 +28,6 @@ def equator_plot(r, phi, data, time=None, index=None, pcm=None, cmap=None, title
     r_plot, phi_plot = np.meshgrid(r_pad,phi_pad)
     pcm = ax.pcolormesh(phi_plot,r_plot,padded_data[:,:], cmap=cmap)
     pmin,pmax = pcm.get_clim()
-
     if center_zero:
         cNorm = matplotlib.colors.TwoSlopeNorm(vmin=pmin, vcenter=0, vmax=pmax)
         logger.info("centering zero: {} -- 0 -- {}".format(pmin, pmax))
@@ -119,51 +98,27 @@ if __name__ == "__main__":
         for file in file_list:
             print('reading in {:s}'.format(file))
             f = h5py.File(file, 'r')
-            r = np.array(f['scales/r/1.5'])
-            theta = np.array(f['scales/theta/1.5'])
-            phi = np.array(f['scales/phi/1.5'])
             t = np.array(f['scales/sim_time'])
-            # pcm_list = []
-            # fig_list = []
-            # for field in fields:
-            #     if field == 's_fluc':
-            #         cmap = 'RdYlBu_r'
-            #         title="s'"
-            #         center_zero=True
-            #         time = t
-            #     else:
-            #         cmap = None
-            #         center_zero=False
-            #         title=None
-            #         time=None
-            #     fig, pcm = equator_plot(r,phi,np.array(f['tasks/{:s} eq'.format(field)][0,:,0]),cmap=cmap,center_zero=center_zero,title=title)
-            #     pcm_list.append(pcm)
-            #     fig_list.append(fig)
-
+            print(f['scales/write_number'][:])
             for k in range(len(t)):
                 for i, field in enumerate(fields):
-                    if field == 's_fluc':
+                    time = t
+                    center_zero=False
+                    title = field
+                    task = f['tasks'][field]
+                    phi = task.dims[1][0][:]
+                    theta = task.dims[2][0][:]
+                    r = task.dims[3][0][:]
+                    i_eq = np.argmin(theta-np.pi/2)
+                    eq_slice = np.squeeze(task[:,:,i_eq,:])
+                    if field == 's':
                         cmap = 'RdYlBu_r'
-                        title="s'"
-                        center_zero=True
-                        time = t
-                        σ = 0.11510794072958948
-                        norm = 1/σ**2
+                        if args['--remove_m0']:
+                            center_zero = True
+                            eq_slice -= np.mean(eq_slice, axis=1, keepdims=True)
+                            title += "'"
                     else:
                         cmap = None
-                        center_zero=False
-                        title=None
-                        time=None
-                        norm=1
-                    fig, pcm = equator_plot(r,phi,np.array(f['tasks/{:s} eq'.format(field)][k,:,0]),time=t[k], cmap=cmap,center_zero=center_zero,title=title, norm=norm)#,pcm=pcm_list[i])
-                    # fig_list[i].savefig('{:s}/{:s}_eq_{:06d}.png'.format(str(output_path),field,f['scales/write_number'][k]), dpi=dpi)
+
+                    fig, pcm = equator_plot(r,phi,eq_slice[k,:,:],time=t[k], cmap=cmap,center_zero=center_zero,title=title)
                     fig.savefig('{:s}/{:s}_eq_{:06d}.png'.format(str(output_path),field,f['scales/write_number'][k]), dpi=dpi)
-    import subprocess
-    with Sync() as sync:
-        if sync.comm.rank == 0 and not args['--no_movie']:
-            fps = int(args['--fps'])
-            for field in fields:
-                print("saving movie {:s}.mp4".format(field))
-                movie_process = subprocess.run("png2mp4 {:s}/{:s} {:s}/../{:s}.mp4 {:d}".format(str(output_path), field, str(output_path), field, fps),
-                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                logger.info(movie_process.stderr)
