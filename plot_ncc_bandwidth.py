dd6741a31f0d77f30e7446066880682a399de556
Benjamin Brown <bpbrown@gmail.com>
2021-11-29 17:00:30 -0800

    Improvements to plot_ncc_bandwidth.py.

--- plot_ncc_bandwidth.py
+++ plot_ncc_bandwidth.py
@@ -10,7 +10,7 @@ dlog.setLevel(logging.WARNING)
 
 ncc_cutoff = 1e-10
 
-LE = lane_emden(64)
+LE = lane_emden(64, dtype=np.float64)
 
 T = LE['T']
 lnρ = LE['lnρ']
@@ -21,14 +21,24 @@ grad = lambda A: de.Gradient(A, c)
 
 lnT = np.log(T).evaluate()
 lnT.name='lnT'
-grad_lnT = grad(lnT).evaluate()
+grad_lnT = (grad(lnT)).evaluate()
 grad_lnT.name='grad_lnT'
 ρ = np.exp(lnρ).evaluate()
 ρ.name='ρ'
-grad_lnρ = grad(lnρ).evaluate()
+grad_lnρ = (grad(lnρ)).evaluate()
 grad_lnρ.name='grad_lnρ'
 ρT = (ρ*T).evaluate()
 ρT.name='ρT'
+
+Tgrad_lnT = (T*grad(lnT)).evaluate()
+Tgrad_lnT.name='Tgrad_lnT'
+
+Tgrad_lnρ = (T*grad(lnρ)).evaluate()
+Tgrad_lnρ.name='Tgrad_lnρ'
+
+logger.info("NCC expansions:")
+for ncc in [ρ, T, ρT, grad_lnT, grad_lnρ, Tgrad_lnT, Tgrad_lnρ]:
+    logger.info("{}: {}".format(ncc, np.where(np.abs(ncc['c']) >= ncc_cutoff)[0].shape))
 
 # the LHS anelastic NCCs are T, grad_lnT, grad_lnρ, ρ_inv
 
@@ -37,9 +47,11 @@ ax[0,0].plot(r[0,0,:], T['g'][0,0,:])
 ax[0,1].plot(r[0,0,:], ρT['g'][0,0,:])
 ax[1,0].plot(r[0,0,:], grad_lnT['g'][2][0,0,:])
 ax[1,1].plot(r[0,0,:], grad_lnρ['g'][2][0,0,:])
+ax[1,0].plot(r[0,0,:], Tgrad_lnT['g'][2][0,0,:])
+ax[1,1].plot(r[0,0,:], Tgrad_lnρ['g'][2][0,0,:])
 ax[0,0].set_ylabel('T')
 ax[0,1].set_ylabel('ρT')
-ax[1,0].set_ylabel('gradT')
+ax[1,0].set_ylabel('gradlnT')
 ax[1,1].set_ylabel('gradlnrho')
 plt.tight_layout()
 fig.savefig('nccs.pdf')
@@ -49,9 +61,11 @@ ax[0,0].plot(np.abs(T['c'][0,0,:]))
 ax[0,1].plot(np.abs(ρT['c'][0,0,:]))
 ax[1,0].plot(np.abs(grad_lnT['c'][1][0,0,:])) # index 1 is spin 0
 ax[1,1].plot(np.abs(grad_lnρ['c'][1][0,0,:])) # index 1 is spin 0
+ax[1,0].plot(np.abs(Tgrad_lnT['c'][1][0,0,:])) # index 1 is spin 0
+ax[1,1].plot(np.abs(Tgrad_lnρ['c'][1][0,0,:])) # index 1 is spin 0
 ax[0,0].set_ylabel('T')
 ax[0,1].set_ylabel('ρT')
-ax[1,0].set_ylabel('gradT')
+ax[1,0].set_ylabel('gradlnT')
 ax[1,1].set_ylabel('gradlnrho')
 for axi in ax:
     for axii in axi:
@@ -59,38 +73,3 @@ for axi in ax:
         axii.set_yscale('log')
 plt.tight_layout()
 fig.savefig('nccs_coeff.pdf')
-
-
-
-T_1 = (1/T).evaluate()
-grad_lnT_1 = (1/grad_lnT).evaluate()
-grad_lnρ_1 = (1/grad_lnρ).evaluate()
-ρT_1 = (1/ρT).evaluate()
-
-fig, ax = plt.subplots(nrows=2, ncols=2)
-ax[0,0].plot(r[0,0,:], T_1['g'][0,0,:])
-ax[0,1].plot(r[0,0,:], ρT_1['g'][0,0,:])
-ax[1,0].plot(r[0,0,:], grad_lnT_1['g'][2][0,0,:])
-ax[1,1].plot(r[0,0,:], grad_lnρ_1['g'][2][0,0,:])
-ax[0,0].set_ylabel('T')
-ax[0,1].set_ylabel('ρT')
-ax[1,0].set_ylabel('gradT')
-ax[1,1].set_ylabel('gradlnrho')
-plt.tight_layout()
-fig.savefig('nccs_1.pdf')
-
-fig, ax = plt.subplots(nrows=2, ncols=2)
-ax[0,0].plot(np.abs(T_1['c'][0,0,:]))
-ax[0,1].plot(np.abs(ρT_1['c'][0,0,:]))
-ax[1,0].plot(np.abs(grad_lnT_1['c'][1][0,0,:])) # index 1 is spin 0
-ax[1,1].plot(np.abs(grad_lnρ_1['c'][1][0,0,:])) # index 1 is spin 0
-ax[0,0].set_ylabel('T')
-ax[0,1].set_ylabel('ρT')
-ax[1,0].set_ylabel('gradT')
-ax[1,1].set_ylabel('gradlnrho')
-for axi in ax:
-    for axii in axi:
-        axii.axhline(y=ncc_cutoff, linestyle='dashed', color='xkcd:grey')
-        axii.set_yscale('log')
-plt.tight_layout()
-fig.savefig('nccs_1_coeff.pdf')
