81481843d6c39d0dbe5a0f77ca5f9115c1118ca2
Benjamin Brown <bpbrown@gmail.com>
2022-12-14 10:46:18 -0800

    Adds plotting of angular momentum coeffs.

--- plot_coeffs.py
+++ plot_coeffs.py
@@ -1,10 +1,11 @@
 """
-Plot slices from joint analysis files.
+Plot coeffs from joint analysis files.
 
 Usage:
     plot_coeffs.py <files>... [options]
 
 Options:
+    --fields=<fields>    Fields to extract coeffs of [default: ρu]
     --output=<output>    Output directory; if blank a guess based on likely case name will be made
     --dpi=<dpi>          dpi for image files (if png) [default: 300]
 """
@@ -25,7 +26,6 @@ if __name__ == "__main__":
     import pathlib
     from docopt import docopt
     from dedalus.tools import post
-    from dedalus.tools.parallel import Sync
 
     import logging
     logger = logging.getLogger(__name__.split('.')[-1])
@@ -35,18 +35,15 @@ if __name__ == "__main__":
         output_path = pathlib.Path(args['--output']).absolute()
     else:
         data_dir = args['<files>'][0].split('/')[0]
-        data_dir += '/frames/'
         output_path = pathlib.Path(data_dir).absolute()
     # Create output directory if needed
-    with Sync() as sync:
-        if sync.comm.rank == 0:
-            if not output_path.exists():
-                output_path.mkdir()
+    if not output_path.exists():
+        output_path.mkdir()
     logger.info("output to {}".format(output_path))
 
     dpi = float(args['--dpi'])
 
-    fields = ['ρu'] #fields = args['--fields'].split(',')
+    fields = args['--fields'].split(',')
 
     def accumulate_files(filename,start,count,file_list):
         if start==0:
@@ -55,22 +52,37 @@ if __name__ == "__main__":
     file_list = []
     post.visit_writes(args['<files>'],  accumulate_files, file_list=file_list)
     logger.info(file_list)
-    if len(file_list) > 0:
-        for file in file_list:
-            print('reading in {:s}'.format(file))
-            f = h5py.File(file, 'r')
-            t = np.array(f['scales/sim_time'])
-            print(f['scales/write_number'][:])
-            for k in range(len(t)):
-                for i, field in enumerate(fields):
-                    time = t
-                    center_zero=False
-                    title = field
-                    task = f['tasks'][field]
-                    # phi = task.dims[1][0][:]
-                    # theta = task.dims[2][0][:]
-                    # r = task.dims[3][0][:]
-                    data_slices = (k, slice(None), slice(None), slice(None), slice(None))
-                    data = task[data_slices]
-                    i=2
-                    print('reg ', i, data[i,0:4,1,0])
+
+    reg = 2
+    times = np.array([])
+    data = []
+    for file in file_list:
+        print('reading in {:s}'.format(file))
+        f = h5py.File(file, 'r')
+        t = np.array(f['scales/sim_time'])
+        times = np.concatenate((times, t))
+        for k in range(len(t)):
+            for i, field in enumerate(fields):
+                task = f['tasks'][field]
+                m = task.dims[2][0][:]
+                ell = task.dims[3][0][:]
+                n = task.dims[4][0][:]
+                mask = ((m == 0) + (m == 1))*(ell == 1)*(n == 0)
+                data.append(task[k][reg][mask])
+    data = np.array(data).T
+
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    print('m:{}, ell:{}, n:{}'.format(m[mask], ell[mask], n[mask]))
+    ax[0].plot(times, data[2,:], label=r'$\ell=1,m=1s,n=0$')
+    ax[0].plot(times, data[3,:], label=r'$\ell=1,m=1c,n=0$')
+    ax[0].plot(times, data[1,:], label=r'$\ell=1,m=0c,n=0$')
+    ax[1].plot(times, np.abs(data[2,:]), label=r'$\ell=1,m=1s,n=0$')
+    ax[1].plot(times, np.abs(data[3,:]), label=r'$\ell=1,m=1c,n=0$')
+    ax[1].plot(times, np.abs(data[1,:]), label=r'$\ell=1,m=0c,n=0$')
+    ax[1].set_xlabel('time')
+    ax[0].set_ylabel(r'$\rho u$')
+    ax[1].set_ylabel(r'$\rho u$')
+    ax[1].set_yscale('log')
+    ax[0].legend()
+    ax[1].legend()
+    fig.savefig(data_dir+'/angular_momentum_coeffs.png', dpi=300)
